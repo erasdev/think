d85b6b4cb0171184bb26d6ce98297f185f8878f2
erica smith <ericasmith2@gmail.com>
2022-11-09 14:43:35 -0500

    WIP

--- think/resources/js/Components/Welcome.vue
+++ think/resources/js/Components/Welcome.vue
@@ -3,5 +3,7 @@
 </script>
 
 <template>
-    <div>dashboard</div>
+    <div class="w-full h-screen overflow-y-scroll lg:w-[calc(100vw-30rem)]] bg-white no-scrollbar">
+        <!--        -->
+    </div>
 </template>
